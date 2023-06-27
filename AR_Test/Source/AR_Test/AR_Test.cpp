33e5db27e2e199413601a0ce70a9e7b85dd36add
pasongsj <audrbekf@gmail.com>
2023-06-27 09:26:14 +0900

    Snake_Level commit

---
+++ AR_Test/Source/AR_Test/AR_Test.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AR_Test.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AR_Test, "AR_Test" );
---
+++ AR_Test/Source/AR_Test/AR_TestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AR_TestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AR_TEST_API AAR_TestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
